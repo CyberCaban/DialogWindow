f6df1e1bbfa9d573be0efb9f69e7a83198206f8c
Dmitry <andrewgamer63@gmail.com>
2024-01-06 21:15:54 +0300

    late fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import ModalDialog from "./components/Modal/index";
 import { dialogStages, dialogs } from "./dialog";
